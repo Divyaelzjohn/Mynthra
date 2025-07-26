c2a23a029cbf8fdf80026f6c1e4b913a870d6d2c
Divya <divelzjon@gmail.com>
2025-07-26 22:45:59 +0530

    Myntra

---
+++ myntra.js
@@ -0,0 +1,42 @@
+import {products} from '/products.js';
+
+let productsHTML = '';
+let products1HTML='';
+
+products.forEach((products) => {
+  productsHTML+= `
+    <li class="list">
+      <div class="list-img">
+        <img src="${products.image}">
+        <div class="rating">
+          <span class="rating-text">${products.rating.text}</span>
+          <img src="${products.rating.star}" class="rating-star">
+          <span style="color: rgb(148, 150, 159);">|</span>
+          <span class="rating-count">${products.rating.count}</span>
+        </div>
+        <div class="info">
+          <h3>${products.info.heading}</h3>
+          <h4>${products.info.heading1}</h4>
+          <div class="price-container">
+            <span class="buy-price">
+              <img src="images/svgexport-15.svg">
+              <span class="price">${products.price}</span>
+            </span>
+            <span class="orig">
+              <img src="${products.origin}">
+              <span class="orig-price">${products.orginalprice}
+              </span>
+            </span>
+            <span class="disc-price">${products.discount}</span>
+          </div>
+          <div class="space"></div>
+        </div>
+        <div class="like">
+          <img src="images/svgexport-17.svg" alt="">
+        </div>
+      </div>
+    </li>
+  `;
+});
+// console.log(productsHTML)
+document.querySelector('.main').innerHTML=productsHTML;
---
+++ products.js
@@ -0,0 +1,139 @@
+export const products=[
+  {
+    id:"e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    image:"images/img/image.png",
+    rating:{
+      text:3.8,
+      star:"images/star.png",
+      count:450
+    },
+    info:{
+      heading:"Athena Immutable",
+      heading1:"Bodycon Midi Dress",
+    },
+    price:574, 
+    origin:"images/svgexport-16.svg",
+    orginalprice:2299,
+    discount:"(75% OFF)"
+  },
+  {
+    id:"15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    image:"images/img/image2.png",
+    rating:{
+      text:4.3,
+      star:"images/star2.png",
+      count:"2.9k",
+    },
+    info:{
+      heading:"Claura",
+      heading1:"Women  Fit & Flare Midi Dress",
+    },
+    price:724, 
+    origin:"images/svgexport-16.svg",
+    orginalprice:2899,
+    discount:"(75% OFF)"
+  },
+  {
+    id:"15b6fc6f-327a-4ec4-896f-486349e85a3e",
+    image:"images/img/img3.webp",
+    rating:{
+      text:4.5,
+      star:"images/star2.png",
+      count:"11"
+    },
+    info:{
+      heading:"MANGO",
+      heading1:"Halter Neck Maxi Dress",
+    },
+    price:3790, 
+    orginalprice:"",
+    discount:""
+  },
+  {
+    id:"15b6fc6f-327a-4ec4-896f-486349e85a3f",
+    image:"images/img/img4.webp",
+    rating:{
+      text:4.1,
+      star:"images/star2.png",
+      count:"337"
+    },
+    info:{
+      heading:"Varanga",
+      heading1:"A-Line Maxi Dress",
+    },
+    price:749, 
+    origin:"images/svgexport-16.svg",
+    orginalprice:2999,
+    discount:"(Rs. 2250 OFF)"
+  },
+  {
+    id:"15b6fc6f-327a-4ec4-896f-486349e85a3g",
+    image:"images/img/img5.webp",
+    rating:{
+      text:2.9,
+      star:"images/star3.png",
+      count:"29"
+    },
+    info:{
+      heading:"Veeralda",
+      heading1:"Shirt Mini Dress",
+    },
+    price:1259, 
+    origin:"images/svgexport-16.svg",
+    orginalprice:1799,
+    discount:"(30% OFF)"
+  },
+  {
+    id:"15b6fc6f-327a-4ec4-896f-486349e85a3h",
+    image:"images/img/img6.webp",
+    rating:{
+      text:4.2,
+      star:"images/star2.png",
+      count:"22"
+    },
+    info:{
+      heading:"MANGO",
+      heading1:"Gathered Sheath Midi Dress",
+    },
+    price:5790, 
+    origin:"",
+    orginalprice:"",
+    discount:""
+  },
+  {
+    id:"15b6fc6f-327a-4ec4-896f-486349e85a3i",
+    image:"images/img/img7.webp",
+    rating:{
+      text:4.2,
+      star:"images/star2.png",
+      count:"283"
+    },
+    info:{
+      heading:"Azira",
+      heading1:"Midi Puff Sleeve Dress",
+    },
+    price:1099, 
+    origin:"images/svgexport-16.svg",
+    orginalprice:4399,
+    discount:"(Rs. 3300 OFF)"
+  },
+  {
+    id:"15b6fc6f-327a-4ec4-896f-486349e85a3j",
+    image:"images/img/img8.webp",
+    rating:{
+      text:4.5,
+      star:"images/star2.png",
+      count:601
+    },
+    info:{
+      heading:"StyleStone",
+      heading1:"Denim Fit & Flare Midi Dress",
+    },
+    price:1299, 
+    origin:"images/svgexport-16.svg",
+    orginalprice:2599,
+    discount:"(50% OFF)"
+  },
+]
+
+
